aee56a299563bc78f74976d554ea5b27b58c5541
roma-bond <bondarenkore@gmail.com>
2020-04-10 03:32:12 -0300

    Структуры данных

---
+++ src/mock/filter.js
@@ -0,0 +1,12 @@
+const filterNames = [`all`, `overdue`, `today`, `favorites`, `repeating`, `archive`];
+
+const generateFilters = () => {
+  return filterNames.map((it) => {
+    return {
+      title: it,
+      count: Math.floor(Math.random() * 10)
+    };
+  });
+};
+
+export default generateFilters;
