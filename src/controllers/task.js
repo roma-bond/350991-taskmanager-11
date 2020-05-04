ea824272492d25ac4c0cbd657f9263be1fbddfda
roma-bond <bondarenkore@gmail.com>
2020-05-04 14:28:45 -0300

    Датабиндинг

---
+++ src/controllers/task.js
@@ -0,0 +1,65 @@
+import TaskComponent from "../components/task.js";
+import TaskEditComponent from "../components/task-editor.js";
+import {render, replace, RenderPosition} from "../utils/render.js";
+
+class TaskController {
+  constructor(container, onDataChange) {
+    this._container = container;
+    this._onDataChange = onDataChange;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  render(task) {
+    this._taskComponent = new TaskComponent(task);
+    this._taskEditComponent = new TaskEditComponent(task);
+
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._taskComponent.setArchiveButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
+    });
+
+    this._taskComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
+    });
+
+    this._taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    render(this._container, this._taskComponent, RenderPosition.BEFORE_END);
+  }
+
+  _replaceTaskToEdit() {
+    replace(this._taskEditComponent, this._taskComponent);
+  }
+
+  _replaceEditToTask() {
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+    replace(this._taskComponent, this._taskEditComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+}
+
+
+export default TaskController;
